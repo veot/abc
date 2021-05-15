fffa42d5347f54dfa9dceb697d35dadaf1c277d6
Otso Velhonoja <48589417+veot@users.noreply.github.com>
2021-05-15 14:36:31 +0300

    Switch to using the Fetch API

--- static/main.js
+++ static/main.js
@@ -1,50 +1,51 @@
 window.onload = () => {
-    const sliders = {
-        'amount': 'amountDisplay',
-        'min': 'minDisplay',
-        'max': 'maxDisplay'
-    };
+  const sliders = {
+    amount: 'amountDisplay',
+    min: 'minDisplay',
+    max: 'maxDisplay',
+  };
 
-    for (const key of Object.keys(sliders)) {
-        displaySlider(key, sliders[key])
-    }
+  for (const key of Object.keys(sliders)) {
+    displaySlider(key, sliders[key]);
+  }
 
-    const submit = document.getElementById('submit-btn')
-    submit.addEventListener('click', draw)
-}
+  const submit = document.getElementById('submit-btn');
+  submit.addEventListener('click', draw);
+};
 
 const displaySlider = (sliderId, displayId) => {
-    const slider = document.getElementById(sliderId);
-    const sliderDisplay = document.getElementById(displayId);
-    sliderDisplay.innerHTML = slider.value;
+  const slider = document.getElementById(sliderId);
+  const sliderDisplay = document.getElementById(displayId);
+  sliderDisplay.innerHTML = slider.value;
 
-    slider.oninput = function() {
-        sliderDisplay.innerHTML = this.value;
-    }
-}
+  slider.oninput = function () {
+    sliderDisplay.innerHTML = this.value;
+  };
+};
 
-const draw = (e) => {
-    const form = document.forms.drawForm;
-    let query = "";
-    if (form.red.checked) {query += 'red=True&'};
-    if (form.green.checked) {query += 'green=True&'};
-    if (form.blue.checked) {query += 'blue=True&'};
-    query += `amount=${form.amount.value}&`;
-    query += `min=${form.min.value}&`;
-    query += `max=${form.max.value}`;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `/draw?${query}`);
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        const data = xhr.response;
-        const src = data.path + '?' + new Date().getTime();
-        const drawing = document.getElementById('drawing');
-        drawing.innerHTML = `<img src=${src} alt="">`;
-        const download = document.getElementById('download-btn');
-        download.setAttribute('href', data.path);
-        download.firstChild.style.cursor = 'pointer';
-    };
-    xhr.send();
-}
-
-
+const draw = () => {
+  const form = document.forms.drawForm;
+  let query = '';
+  form.red.checked ? (query += 'red=true&') : (query += 'red=false&');
+  form.green.checked ? (query += 'green=true&') : (query += 'green=false&');
+  form.blue.checked ? (query += 'blue=true&') : (query += 'blue=false&');
+  query += `amount=${form.amount.value}&`;
+  query += `min_r=${form.min.value}&`;
+  query += `max_r=${form.max.value}`;
+  const xhr = new XMLHttpRequest();
+  fetch(`/draw?${query}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      const src = data.path;
+      const drawing = document.getElementById('drawing');
+      drawing.innerHTML = `<img src=${src} alt="generated image">`;
+      const download = document.getElementById('download-btn');
+      download.setAttribute('href', data.path);
+      download.firstChild.style.cursor = 'pointer';
+    })
+    .catch((err) => {
+      console.log('Something went wrong', err);
+    });
+};
